17d2bb7a4ef51c753988b6185f2048a5181862fa
Petr Babanov <petr.babanov@softcom.su>
2021-12-13 21:17:00 +0300

    Refactored structure, added tests

---
+++ src/test/resources/org/polystat/py2eo/afterHeapify/trivial.py
@@ -0,0 +1,169 @@
+from heapifyRuntime import *
+testFib1 = indirHeap.new(0)
+def tmpFun0(closure, /):
+    pass
+    indirHeap.set(closure[19], valuesHeap.new(0))
+    return allFuns[valuesHeap.get(indirHeap.get(closure[12]))[0]](valuesHeap.get(indirHeap.get(closure[12])))
+def tmpFun1(closure, /):
+    pass
+    indirHeap.set(closure[3], valuesHeap.new(1))
+    return allFuns[valuesHeap.get(indirHeap.get(closure[23]))[0]](valuesHeap.get(indirHeap.get(closure[23])))
+def tmpFun2(closure, /):
+    pass
+    indirHeap.set(closure[25], valuesHeap.new(0))
+    return allFuns[valuesHeap.get(indirHeap.get(closure[4]))[0]](valuesHeap.get(indirHeap.get(closure[4])))
+def tmpFun3(closure, /):
+    pass
+    if (True):
+        return allFuns[valuesHeap.get(indirHeap.get(closure[16]))[0]](valuesHeap.get(indirHeap.get(closure[16])))
+    else:
+        return allFuns[valuesHeap.get(indirHeap.get(closure[2]))[0]](valuesHeap.get(indirHeap.get(closure[2])))
+def tmpFun4(closure, /):
+    pass
+    indirHeap.set(closure[21], valuesHeap.new((valuesHeap.get(indirHeap.get(closure[25])) < 10)))
+    return allFuns[valuesHeap.get(indirHeap.get(closure[7]))[0]](valuesHeap.get(indirHeap.get(closure[7])))
+def tmpFun5(closure, /):
+    pass
+    if (not valuesHeap.get(indirHeap.get(closure[21]))):
+        return allFuns[valuesHeap.get(indirHeap.get(closure[14]))[0]](valuesHeap.get(indirHeap.get(closure[14])))
+    else:
+        return allFuns[valuesHeap.get(indirHeap.get(closure[20]))[0]](valuesHeap.get(indirHeap.get(closure[20])))
+def tmpFun6(closure, /):
+    pass
+    return allFuns[valuesHeap.get(indirHeap.get(closure[5]))[0]](valuesHeap.get(indirHeap.get(closure[5])))
+def tmpFun7(closure, /):
+    pass
+    pass
+    return allFuns[valuesHeap.get(indirHeap.get(closure[10]))[0]](valuesHeap.get(indirHeap.get(closure[10])))
+def tmpFun8(closure, /):
+    pass
+    indirHeap.set(closure[18], valuesHeap.new((valuesHeap.get(indirHeap.get(closure[19])) + valuesHeap.get(indirHeap.get(closure[3])))))
+    return allFuns[valuesHeap.get(indirHeap.get(closure[17]))[0]](valuesHeap.get(indirHeap.get(closure[17])))
+def tmpFun9(closure, /):
+    pass
+    indirHeap.set(closure[11], valuesHeap.new(valuesHeap.get(indirHeap.get(closure[18]))))
+    return allFuns[valuesHeap.get(indirHeap.get(closure[1]))[0]](valuesHeap.get(indirHeap.get(closure[1])))
+def tmpFun10(closure, /):
+    pass
+    indirHeap.set(closure[13], valuesHeap.new((valuesHeap.get(indirHeap.get(closure[25])) + 1)))
+    return allFuns[valuesHeap.get(indirHeap.get(closure[15]))[0]](valuesHeap.get(indirHeap.get(closure[15])))
+def tmpFun11(closure, /):
+    pass
+    indirHeap.set(closure[25], valuesHeap.new(valuesHeap.get(indirHeap.get(closure[13]))))
+    return allFuns[valuesHeap.get(indirHeap.get(closure[26]))[0]](valuesHeap.get(indirHeap.get(closure[26])))
+def tmpFun12(closure, /):
+    pass
+    indirHeap.set(closure[19], valuesHeap.new(valuesHeap.get(indirHeap.get(closure[3]))))
+    return allFuns[valuesHeap.get(indirHeap.get(closure[24]))[0]](valuesHeap.get(indirHeap.get(closure[24])))
+def tmpFun13(closure, /):
+    pass
+    indirHeap.set(closure[3], valuesHeap.new(valuesHeap.get(indirHeap.get(closure[11]))))
+    return allFuns[valuesHeap.get(indirHeap.get(closure[4]))[0]](valuesHeap.get(indirHeap.get(closure[4])))
+def tmpFun14(closure, /):
+    pass
+    pass
+    return allFuns[valuesHeap.get(indirHeap.get(closure[5]))[0]](valuesHeap.get(indirHeap.get(closure[5])))
+def tmpFun15(closure, /):
+    pass
+    indirHeap.set(closure[6], valuesHeap.new((55 == valuesHeap.get(indirHeap.get(closure[19])))))
+    return allFuns[valuesHeap.get(indirHeap.get(closure[8]))[0]](valuesHeap.get(indirHeap.get(closure[8])))
+def tmpFun16(closure, /):
+    pass
+    return valuesHeap.get(indirHeap.get(closure[6]))
+    return allFuns[valuesHeap.get(indirHeap.get(closure[9]))[0]](valuesHeap.get(indirHeap.get(closure[9])))
+def tmpFun17(closure, /):
+    return None
+def tmpFun18(closure, /):
+    bb_interm6 = indirHeap.new(0)
+    bb_else0 = indirHeap.new(0)
+    f1 = indirHeap.new(0)
+    bb_interm2 = indirHeap.new(0)
+    bb_interm3 = indirHeap.new(0)
+    e3 = indirHeap.new(0)
+    bb_interm4 = indirHeap.new(0)
+    bb_interm11 = indirHeap.new(0)
+    bb_finish0 = indirHeap.new(0)
+    bb_interm5 = indirHeap.new(0)
+    f2 = indirHeap.new(0)
+    bb_interm0 = indirHeap.new(0)
+    e1 = indirHeap.new(0)
+    bb_yes0 = indirHeap.new(0)
+    bb_interm9 = indirHeap.new(0)
+    bb_body0 = indirHeap.new(0)
+    bb_interm7 = indirHeap.new(0)
+    e0 = indirHeap.new(0)
+    f0 = indirHeap.new(0)
+    bb_no0 = indirHeap.new(0)
+    e2 = indirHeap.new(0)
+    bb_start0 = indirHeap.new(0)
+    bb_interm1 = indirHeap.new(0)
+    bb_interm10 = indirHeap.new(0)
+    ii = indirHeap.new(0)
+    bb_interm8 = indirHeap.new(0)
+    indirHeap.set(e1, valuesHeap.new(None))
+    indirHeap.set(f1, valuesHeap.new(None))
+    indirHeap.set(e3, valuesHeap.new(None))
+    indirHeap.set(e2, valuesHeap.new(None))
+    indirHeap.set(ii, valuesHeap.new(None))
+    indirHeap.set(f2, valuesHeap.new(None))
+    indirHeap.set(e0, valuesHeap.new(None))
+    indirHeap.set(f0, valuesHeap.new(None))
+    indirHeap.set(bb_start0, valuesHeap.new(None))
+    indirHeap.set(bb_interm0, valuesHeap.new(None))
+    indirHeap.set(bb_interm1, valuesHeap.new(None))
+    indirHeap.set(bb_interm2, valuesHeap.new(None))
+    indirHeap.set(bb_body0, valuesHeap.new(None))
+    indirHeap.set(bb_interm4, valuesHeap.new(None))
+    indirHeap.set(bb_yes0, valuesHeap.new(None))
+    indirHeap.set(bb_no0, valuesHeap.new(None))
+    indirHeap.set(bb_interm5, valuesHeap.new(None))
+    indirHeap.set(bb_interm7, valuesHeap.new(None))
+    indirHeap.set(bb_interm6, valuesHeap.new(None))
+    indirHeap.set(bb_interm9, valuesHeap.new(None))
+    indirHeap.set(bb_interm8, valuesHeap.new(None))
+    indirHeap.set(bb_interm10, valuesHeap.new(None))
+    indirHeap.set(bb_else0, valuesHeap.new(None))
+    indirHeap.set(bb_interm3, valuesHeap.new(None))
+    indirHeap.set(bb_interm11, valuesHeap.new(None))
+    indirHeap.set(bb_finish0, valuesHeap.new(None))
+    nextClosure = [0, bb_interm6, bb_else0, f1, bb_interm2, bb_interm3, e3, bb_interm4, bb_interm11, bb_finish0, bb_interm5, f2, bb_interm0, e1, bb_yes0, bb_interm9, bb_body0, bb_interm7, e0, f0, bb_no0, e2, bb_start0, bb_interm1, bb_interm10, ii, bb_interm8]
+    indirHeap.set(bb_start0, valuesHeap.new(nextClosure))
+    nextClosure = [1, bb_interm6, bb_else0, f1, bb_interm2, bb_interm3, e3, bb_interm4, bb_interm11, bb_finish0, bb_interm5, f2, bb_interm0, e1, bb_yes0, bb_interm9, bb_body0, bb_interm7, e0, f0, bb_no0, e2, bb_start0, bb_interm1, bb_interm10, ii, bb_interm8]
+    indirHeap.set(bb_interm0, valuesHeap.new(nextClosure))
+    nextClosure = [2, bb_interm6, bb_else0, f1, bb_interm2, bb_interm3, e3, bb_interm4, bb_interm11, bb_finish0, bb_interm5, f2, bb_interm0, e1, bb_yes0, bb_interm9, bb_body0, bb_interm7, e0, f0, bb_no0, e2, bb_start0, bb_interm1, bb_interm10, ii, bb_interm8]
+    indirHeap.set(bb_interm1, valuesHeap.new(nextClosure))
+    nextClosure = [3, bb_interm6, bb_else0, f1, bb_interm2, bb_interm3, e3, bb_interm4, bb_interm11, bb_finish0, bb_interm5, f2, bb_interm0, e1, bb_yes0, bb_interm9, bb_body0, bb_interm7, e0, f0, bb_no0, e2, bb_start0, bb_interm1, bb_interm10, ii, bb_interm8]
+    indirHeap.set(bb_interm2, valuesHeap.new(nextClosure))
+    nextClosure = [4, bb_interm6, bb_else0, f1, bb_interm2, bb_interm3, e3, bb_interm4, bb_interm11, bb_finish0, bb_interm5, f2, bb_interm0, e1, bb_yes0, bb_interm9, bb_body0, bb_interm7, e0, f0, bb_no0, e2, bb_start0, bb_interm1, bb_interm10, ii, bb_interm8]
+    indirHeap.set(bb_body0, valuesHeap.new(nextClosure))
+    nextClosure = [5, bb_interm6, bb_else0, f1, bb_interm2, bb_interm3, e3, bb_interm4, bb_interm11, bb_finish0, bb_interm5, f2, bb_interm0, e1, bb_yes0, bb_interm9, bb_body0, bb_interm7, e0, f0, bb_no0, e2, bb_start0, bb_interm1, bb_interm10, ii, bb_interm8]
+    indirHeap.set(bb_interm4, valuesHeap.new(nextClosure))
+    nextClosure = [6, bb_interm6, bb_else0, f1, bb_interm2, bb_interm3, e3, bb_interm4, bb_interm11, bb_finish0, bb_interm5, f2, bb_interm0, e1, bb_yes0, bb_interm9, bb_body0, bb_interm7, e0, f0, bb_no0, e2, bb_start0, bb_interm1, bb_interm10, ii, bb_interm8]
+    indirHeap.set(bb_yes0, valuesHeap.new(nextClosure))
+    nextClosure = [7, bb_interm6, bb_else0, f1, bb_interm2, bb_interm3, e3, bb_interm4, bb_interm11, bb_finish0, bb_interm5, f2, bb_interm0, e1, bb_yes0, bb_interm9, bb_body0, bb_interm7, e0, f0, bb_no0, e2, bb_start0, bb_interm1, bb_interm10, ii, bb_interm8]
+    indirHeap.set(bb_no0, valuesHeap.new(nextClosure))
+    nextClosure = [8, bb_interm6, bb_else0, f1, bb_interm2, bb_interm3, e3, bb_interm4, bb_interm11, bb_finish0, bb_interm5, f2, bb_interm0, e1, bb_yes0, bb_interm9, bb_body0, bb_interm7, e0, f0, bb_no0, e2, bb_start0, bb_interm1, bb_interm10, ii, bb_interm8]
+    indirHeap.set(bb_interm5, valuesHeap.new(nextClosure))
+    nextClosure = [9, bb_interm6, bb_else0, f1, bb_interm2, bb_interm3, e3, bb_interm4, bb_interm11, bb_finish0, bb_interm5, f2, bb_interm0, e1, bb_yes0, bb_interm9, bb_body0, bb_interm7, e0, f0, bb_no0, e2, bb_start0, bb_interm1, bb_interm10, ii, bb_interm8]
+    indirHeap.set(bb_interm7, valuesHeap.new(nextClosure))
+    nextClosure = [10, bb_interm6, bb_else0, f1, bb_interm2, bb_interm3, e3, bb_interm4, bb_interm11, bb_finish0, bb_interm5, f2, bb_interm0, e1, bb_yes0, bb_interm9, bb_body0, bb_interm7, e0, f0, bb_no0, e2, bb_start0, bb_interm1, bb_interm10, ii, bb_interm8]
+    indirHeap.set(bb_interm6, valuesHeap.new(nextClosure))
+    nextClosure = [11, bb_interm6, bb_else0, f1, bb_interm2, bb_interm3, e3, bb_interm4, bb_interm11, bb_finish0, bb_interm5, f2, bb_interm0, e1, bb_yes0, bb_interm9, bb_body0, bb_interm7, e0, f0, bb_no0, e2, bb_start0, bb_interm1, bb_interm10, ii, bb_interm8]
+    indirHeap.set(bb_interm9, valuesHeap.new(nextClosure))
+    nextClosure = [12, bb_interm6, bb_else0, f1, bb_interm2, bb_interm3, e3, bb_interm4, bb_interm11, bb_finish0, bb_interm5, f2, bb_interm0, e1, bb_yes0, bb_interm9, bb_body0, bb_interm7, e0, f0, bb_no0, e2, bb_start0, bb_interm1, bb_interm10, ii, bb_interm8]
+    indirHeap.set(bb_interm8, valuesHeap.new(nextClosure))
+    nextClosure = [13, bb_interm6, bb_else0, f1, bb_interm2, bb_interm3, e3, bb_interm4, bb_interm11, bb_finish0, bb_interm5, f2, bb_interm0, e1, bb_yes0, bb_interm9, bb_body0, bb_interm7, e0, f0, bb_no0, e2, bb_start0, bb_interm1, bb_interm10, ii, bb_interm8]
+    indirHeap.set(bb_interm10, valuesHeap.new(nextClosure))
+    nextClosure = [14, bb_interm6, bb_else0, f1, bb_interm2, bb_interm3, e3, bb_interm4, bb_interm11, bb_finish0, bb_interm5, f2, bb_interm0, e1, bb_yes0, bb_interm9, bb_body0, bb_interm7, e0, f0, bb_no0, e2, bb_start0, bb_interm1, bb_interm10, ii, bb_interm8]
+    indirHeap.set(bb_else0, valuesHeap.new(nextClosure))
+    nextClosure = [15, bb_interm6, bb_else0, f1, bb_interm2, bb_interm3, e3, bb_interm4, bb_interm11, bb_finish0, bb_interm5, f2, bb_interm0, e1, bb_yes0, bb_interm9, bb_body0, bb_interm7, e0, f0, bb_no0, e2, bb_start0, bb_interm1, bb_interm10, ii, bb_interm8]
+    indirHeap.set(bb_interm3, valuesHeap.new(nextClosure))
+    nextClosure = [16, bb_interm6, bb_else0, f1, bb_interm2, bb_interm3, e3, bb_interm4, bb_interm11, bb_finish0, bb_interm5, f2, bb_interm0, e1, bb_yes0, bb_interm9, bb_body0, bb_interm7, e0, f0, bb_no0, e2, bb_start0, bb_interm1, bb_interm10, ii, bb_interm8]
+    indirHeap.set(bb_interm11, valuesHeap.new(nextClosure))
+    nextClosure = [17, bb_interm6, bb_else0, f1, bb_interm2, bb_interm3, e3, bb_interm4, bb_interm11, bb_finish0, bb_interm5, f2, bb_interm0, e1, bb_yes0, bb_interm9, bb_body0, bb_interm7, e0, f0, bb_no0, e2, bb_start0, bb_interm1, bb_interm10, ii, bb_interm8]
+    indirHeap.set(bb_finish0, valuesHeap.new(nextClosure))
+    return allFuns[valuesHeap.get(indirHeap.get(bb_start0))[0]](valuesHeap.get(indirHeap.get(bb_start0)))
+allFuns = [tmpFun0, tmpFun1, tmpFun2, tmpFun3, tmpFun4, tmpFun5, tmpFun6, tmpFun7, tmpFun8, tmpFun9, tmpFun10, tmpFun11, tmpFun12, tmpFun13, tmpFun14, tmpFun15, tmpFun16, tmpFun17, tmpFun18]
+nextClosure = [18,]
+indirHeap.set(testFib1, valuesHeap.new(nextClosure))
+assert allFuns[valuesHeap.get(indirHeap.get(testFib1))[0]](None)
---
+++ src/test/resources/org/polystat/py2eo/afterSimplifyIf/twoFuns.py
@@ -0,0 +1,17 @@
+import builtins as builtins
+from builtins import *
+import sys as sys
+def twoFuns():
+    pass
+    with True:
+        class p():
+            pass
+    class g():
+        pass
+    class g():
+        pass
+    def f():
+        return 1
+    def f():
+        return 2
+    return (2 == f() )
