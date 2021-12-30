34b33ad298e7d4aea7d5a41a45a800338df84fad
dours <akadours2@gmail.com>
2021-12-31 00:32:31 +0200

    ameliorate style

--- src/main/scala/org/polystat/py2eo/SimpleAnalysis.scala
+++ src/main/scala/org/polystat/py2eo/SimpleAnalysis.scala
@@ -70,9 +70,10 @@ object SimpleAnalysis {
       case Assert(x, _) => (List(), x.map(isRhs))
       case Raise(e, from, _) => (List(), (e.toList ++ from.toList).map(isRhs))
       case ClassDef(_, bases, body, decorators, _) => (List(body), (bases.map(_._2) ++ decorators.l).map(isRhs))
-      case FuncDef(_, args, _, _, returnAnnotation, body, decorators, _, _, _) =>
-        (List(body),
-          (decorators.l ++ returnAnnotation.toList ++ args.flatMap(p => p.paramAnn.toList ++ p.default.toList)).map(isRhs))
+      case FuncDef(_, args, _, _, returnAnnotation, body, decorators, _, _, _) => (
+        List(body),
+          (decorators.l ++ returnAnnotation.toList ++ args.flatMap(p => p.paramAnn.toList ++ p.default.toList)).map(isRhs)
+      )
       case NonLocal(_, _) | Pass(_) | Break(_) | Continue(_) | Global(_, _) | ImportModule(_, _, _)
            | ImportSymbol(_, _, _, _) | ImportAllSymbols(_, _) => (List(), List())
       case x : Unsupported => (x.sts, x.es)
@@ -84,7 +85,9 @@ object SimpleAnalysis {
       val (ls, le) = childrenS(s)
       val acc = le.map(_._2).foldLeft(acc0)(foldEE(f))
       ls.foldLeft(acc)(foldSE(f, mayVisit))
-    } else acc0
+    } else {
+      acc0
+    }
   }
 
   // almost a typical fold, but f may disallow it to visit children of certain nodes
@@ -104,10 +107,11 @@ object SimpleAnalysis {
     }
     type H = HashMap[String, (VarScope.T, GeneralAnnotation)]
     val h2 = foldSS[H]((h, st) => st match {
-      case NonLocal(l, ann) => (l.foldLeft(h)((h, name) => h.+((name, (VarScope.NonLocal, ann.pos)))), false)
-      case Global(l, ann)  => (l.foldLeft(h)((h, name) => h.+((name, (VarScope.Global, ann.pos)))), false)
-      case _ => (h, dontVisitOtherBlocks(st))
-    })(HashMap[String, (VarScope.T, GeneralAnnotation)](), body)
+        case NonLocal(l, ann) => (l.foldLeft(h)((h, name) => h.+((name, (VarScope.NonLocal, ann.pos)))), false)
+        case Global(l, ann)  => (l.foldLeft(h)((h, name) => h.+((name, (VarScope.Global, ann.pos)))), false)
+        case _ => (h, dontVisitOtherBlocks(st))
+      }
+    )(HashMap[String, (VarScope.T, GeneralAnnotation)](), body)
 
     val h3 = foldSS[H]((h, st) => {
       def add0(h : H, name : String, ann : GeneralAnnotation) : H =
@@ -118,9 +122,9 @@ object SimpleAnalysis {
         case SimpleObject(name, _, ann) => (add(name, ann), false)
         case FuncDef(name, _, _, _, _, _, _, _, _, ann) => (add(name, ann), false)
         case Assign(List(CollectionCons(_, _, _), _), _) =>
-          throw new Throwable("run this analysis after all assignment simplification passes!")
+          throw new RuntimeException("run this analysis after all assignment simplification passes!")
         case Assign(l, _) if l.size > 2 =>
-          throw new Throwable("run this analysis after all assignment simplification passes!")
+          throw new RuntimeException("run this analysis after all assignment simplification passes!")
         case Assign(List(Ident(name, _), _), ann) => (add(name, ann), true)
         case AnnAssign(Ident(name, _), _, _, ann) => (add(name, ann), true)
         case u : Unsupported => (u.declareVars.foldLeft(h)(add0(_, _, u.ann)), true)
@@ -131,16 +135,6 @@ object SimpleAnalysis {
 
     args.foldLeft(h3)((h, name) => h.+((name.name, (VarScope.Arg, name.ann))))
 
-//    foldSE[H]((h, e) => e match {
-//      case Ident(name) => if (h.contains(name)) h else {
-////        if (!BuiltinFunctions.fs.contains(name))  // todo: this should be the builtin module's global namespace
-//          h.+((name, VarScope.Global))
-////        else
-////        h
-//      }
-//      case _ => h
-//    }, dontVisitOtherBlocks)(h3, body)
-//
   }
 
   private def computeAccessibleIdentsF(upperVars : HashMap[String, (VarScope.T, GeneralAnnotation)], f : FuncDef) : FuncDef = {
@@ -149,42 +143,46 @@ object SimpleAnalysis {
       if (x._2._1 == VarScope.Local || x._2._1 == VarScope.Arg) (x._1, (VarScope.ImplicitNonLocal, x._2._2)) else x)
     val merged = v.foldLeft(vUpper)((acc, z) => acc.+(z))
     val (body, _) = SimplePass.procStatementGeneral((s, ns) => s match {
-      case f : FuncDef => (computeAccessibleIdentsF(merged, f), ns, false)
-      case _ => (s, ns, true)
-    })(f.body, new SimplePass.Names())
-    FuncDef(f.name, f.args, f.otherPositional, f.otherKeyword, f.returnAnnotation,
-      body, f.decorators, merged, f.isAsync, f.ann.pos)
+        case f : FuncDef => (computeAccessibleIdentsF(merged, f), ns, false)
+        case _ => (s, ns, true)
+      }
+    )(f.body, new SimplePass.Names())
+    FuncDef(
+      f.name, f.args, f.otherPositional, f.otherKeyword, f.returnAnnotation,
+      body, f.decorators, merged, f.isAsync, f.ann.pos
+    )
   }
 
   def computeAccessibleIdents(s : Statement) : Statement = {
     SimplePass.procStatementGeneral((s, ns) => s match {
-      case f : FuncDef => (computeAccessibleIdentsF(HashMap(), f), ns, false)
-      case _ => (s, ns, true)
-    })(s, new SimplePass.Names())._1
+        case f : FuncDef => (computeAccessibleIdentsF(HashMap(), f), ns, false)
+        case _ => (s, ns, true)
+      }
+    )(s, new SimplePass.Names())._1
   }
 
   private def assertStatementIsSimplified(acc : Unit, s : Statement) : (Unit, Boolean) = s match {
     case
-//      Del(Ident(_)) | Try(_, List((None, _)), _, _) | AugAssign(_, _, _) |
-//      Raise(_, None) | ClassDef(_, _, _, Decorators(List())) | Global(_) |
       IfSimple(_, _, _, _) | While(_, _, _, _) |
       Suite(_, _) | Assign(List(_), _) |
       Return(_, _) |
       FuncDef(_, _, _, _, _, _, Decorators(List()), _, _, _) |
       NonLocal(_, _) | Pass(_) | Break(_) | Continue(_) | ImportModule(_, _, _) |
-      ImportSymbol(_, _, _, _) | ImportAllSymbols(_, _) => (acc, true)
+      ImportSymbol(_, _, _, _) | ImportAllSymbols(_, _) =>
+        (acc, true)
     case Assign(List(lhs, _), _) if PrintLinearizedMutableEOWithCage.isSeqOfFields(lhs)  => (acc, true)
-    case ClassDef(_, List(), body, Decorators(List()), _) =>
+    case ClassDef(_, List(), body, Decorators(List()), _) => {
       val (Suite(defs, _), _) = SimplePass.procStatement(SimplePass.unSuite)(body, SimplePass.Names(HashMap()))
-      assert(defs.forall{ case Assign(List(Ident(_, _), _), _) => true case _ => false })
+      assert(defs.forall { case Assign(List(Ident(_, _), _), _) => true case _ => false })
       (acc, true)
+    }
   }
 
   private def assertExpressionIsSimplified(acc : Unit, e : T) : Unit = e match {
     case FreakingComparison(List(_), List(_, _), _) => ()
     case Star(_, _) | DoubleStar(_, _) | CollectionComprehension(_, _, _, _) | DictComprehension(_, _, _)
          | CallIndex(false, _, _, _) | FreakingComparison(_, _, _) | AnonFun(_, _, _, _, _) =>
-      throw new Throwable("these must never happen after all passes: " + PrintPython.printExpr(e))
+      throw new RuntimeException("these must never happen after all passes: " + PrintPython.printExpr(e))
     case _ => ()
   }
 
