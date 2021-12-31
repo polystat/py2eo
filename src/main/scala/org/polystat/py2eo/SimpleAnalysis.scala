f60be6c59e64b652745275d3f248990081ac2d60
dours <akadours2@gmail.com>
2021-12-31 18:03:57 +0200

    fix brackets, rm empty lines from methods

--- src/main/scala/org/polystat/py2eo/SimpleAnalysis.scala
+++ src/main/scala/org/polystat/py2eo/SimpleAnalysis.scala
@@ -34,7 +34,7 @@ object SimpleAnalysis {
     case Cond(cond, yes, no, _) => List(cond, yes, no)
     case x : UnsupportedExpr => x.children
     case IntLiteral(_, _) | FloatLiteral(_, _) | StringLiteral(_, _) | BoolLiteral(_, _)
-         | NoneLiteral(_) | ImagLiteral(_, _) | Ident(_, _) | EllipsisLiteral(_) =>
+      | NoneLiteral(_) | ImagLiteral(_, _) | Ident(_, _) | EllipsisLiteral(_) =>
       List()
     case CollectionComprehension(_, base, l, _) => base :: l.flatMap(childrenComprehension)
     case DictComprehension(base, l, _) =>
@@ -72,10 +72,11 @@ object SimpleAnalysis {
       case ClassDef(_, bases, body, decorators, _) => (List(body), (bases.map(_._2) ++ decorators.l).map(isRhs))
       case FuncDef(_, args, _, _, returnAnnotation, body, decorators, _, _, _) => (
         List(body),
-          (decorators.l ++ returnAnnotation.toList ++ args.flatMap(p => p.paramAnn.toList ++ p.default.toList)).map(isRhs)
+        (decorators.l ++ returnAnnotation.toList ++ args.flatMap(p => p.paramAnn.toList ++ p.default.toList)).map(isRhs)
       )
       case NonLocal(_, _) | Pass(_) | Break(_) | Continue(_) | Global(_, _) | ImportModule(_, _, _)
-           | ImportSymbol(_, _, _, _) | ImportAllSymbols(_, _) => (List(), List())
+        | ImportSymbol(_, _, _, _) | ImportAllSymbols(_, _) =>
+        (List(), List())
       case x : Unsupported => (x.sts, x.es)
     }
   }
@@ -101,49 +102,50 @@ object SimpleAnalysis {
 
   private def classifyVariablesAssignedInFunctionBody(args : List[Parameter], body : Statement)
   : HashMap[String, (VarScope.T, GeneralAnnotation)] = {
+    type H = HashMap[String, (VarScope.T, GeneralAnnotation)]
     def dontVisitOtherBlocks(s : Statement) : Boolean = s match {
       case FuncDef(_, _, _, _, _, _, _, _, _, _) | ClassDef(_, _, _, _, _) => false
       case _ => true
     }
-    type H = HashMap[String, (VarScope.T, GeneralAnnotation)]
-    val h2 = foldSS[H]((h, st) => 
-      st match {
+    val allNonLocalAndGlobalNames = foldSS[H](
+      (h, st) => st match {
         case NonLocal(l, ann) => (l.foldLeft(h)((h, name) => h.+((name, (VarScope.NonLocal, ann.pos)))), false)
-        case Global(l, ann)  => (l.foldLeft(h)((h, name) => h.+((name, (VarScope.Global, ann.pos)))), false)
-        case _ => (h, dontVisitOtherBlocks(st))
+        case Global(l, ann)   => (l.foldLeft(h)((h, name) => h.+((name, (VarScope.Global, ann.pos)))), false)
+        case _                => (h, dontVisitOtherBlocks(st))
       }
     )(HashMap[String, (VarScope.T, GeneralAnnotation)](), body)
-
-    val h3 = foldSS[H]((h, st) => {
-      def add0(h : H, name : String, ann : GeneralAnnotation) : H =
-        if (h.contains(name)) h else h.+((name, (VarScope.Local, ann.pos)))
-      def add(name : String, ann : GeneralAnnotation) = add0(h, name, ann)
-      st match {
-        case ClassDef(name, _, _, _, ann) => (add(name, ann), false)
-        case SimpleObject(name, _, ann) => (add(name, ann), false)
-        case FuncDef(name, _, _, _, _, _, _, _, _, ann) => (add(name, ann), false)
-        case Assign(List(CollectionCons(_, _, _), _), _) =>
-          throw new RuntimeException("run this analysis after all assignment simplification passes!")
-        case Assign(l, _) if l.size > 2 =>
-          throw new RuntimeException("run this analysis after all assignment simplification passes!")
-        case Assign(List(Ident(name, _), _), ann) => (add(name, ann), true)
-        case AnnAssign(Ident(name, _), _, _, ann) => (add(name, ann), true)
-        case u : Unsupported => (u.declareVars.foldLeft(h)(add0(_, _, u.ann)), true)
-        case CreateConst(name, _, ann) => (add(name, ann), true)
-        case _ => (h, true)
+    val allNonLocalAndGlobalAndLocalNames = foldSS[H](
+      (h, st) => {
+        def add0(h : H, name : String, ann : GeneralAnnotation) : H =
+          if (h.contains(name)) h else h.+((name, (VarScope.Local, ann.pos)))
+        def add(name : String, ann : GeneralAnnotation) = add0(h, name, ann)
+        st match {
+          case ClassDef(name, _, _, _, ann) => (add(name, ann), false)
+          case SimpleObject(name, _, ann) => (add(name, ann), false)
+          case FuncDef(name, _, _, _, _, _, _, _, _, ann)  => (add(name, ann), false)
+          case Assign(List(CollectionCons(_, _, _), _), _) =>
+            throw new RuntimeException("run this analysis after all assignment simplification passes!")
+          case Assign(l, _) if l.size > 2 =>
+            throw new RuntimeException("run this analysis after all assignment simplification passes!")
+          case Assign(List(Ident(name, _), _), ann) => (add(name, ann), true)
+          case AnnAssign(Ident(name, _), _, _, ann) => (add(name, ann), true)
+          case u : Unsupported => (u.declareVars.foldLeft(h)(add0(_, _, u.ann)), true)
+          case CreateConst(name, _, ann) => (add(name, ann), true)
+          case _ => (h, true)
+        }
       }
-    })(h2, body)
-
-    args.foldLeft(h3)((h, name) => h.+((name.name, (VarScope.Arg, name.ann))))
-
+    )(allNonLocalAndGlobalNames, body)
+    args.foldLeft(allNonLocalAndGlobalAndLocalNames)((h, name) => h.+((name.name, (VarScope.Arg, name.ann))))
   }
 
   private def computeAccessibleIdentsF(upperVars : HashMap[String, (VarScope.T, GeneralAnnotation)], f : FuncDef) : FuncDef = {
     val v = classifyVariablesAssignedInFunctionBody(f.args, f.body)
-    val vUpper = upperVars.map(x =>
-      if (x._2._1 == VarScope.Local || x._2._1 == VarScope.Arg) (x._1, (VarScope.ImplicitNonLocal, x._2._2)) else x)
+    val vUpper = upperVars.map(
+      x => if (x._2._1 == VarScope.Local || x._2._1 == VarScope.Arg) (x._1, (VarScope.ImplicitNonLocal, x._2._2)) else x
+    )
     val merged = v.foldLeft(vUpper)((acc, z) => acc.+(z))
-    val (body, _) = SimplePass.procStatementGeneral((s, ns) => s match {
+    val (body, _) = SimplePass.procStatementGeneral(
+      (s, ns) => s match {
         case f : FuncDef => (computeAccessibleIdentsF(merged, f), ns, false)
         case _ => (s, ns, true)
       }
@@ -155,7 +157,8 @@ object SimpleAnalysis {
   }
 
   def computeAccessibleIdents(s : Statement) : Statement = {
-    SimplePass.procStatementGeneral((s, ns) => s match {
+    SimplePass.procStatementGeneral(
+      (s, ns) => s match {
         case f : FuncDef => (computeAccessibleIdentsF(HashMap(), f), ns, false)
         case _ => (s, ns, true)
       }
@@ -164,14 +167,13 @@ object SimpleAnalysis {
 
   private def assertStatementIsSimplified(acc : Unit, s : Statement) : (Unit, Boolean) = s match {
     case
-      IfSimple(_, _, _, _) | While(_, _, _, _) |
-      Suite(_, _) | Assign(List(_), _) |
-      Return(_, _) |
-      FuncDef(_, _, _, _, _, _, Decorators(List()), _, _, _) |
-      NonLocal(_, _) | Pass(_) | Break(_) | Continue(_) | ImportModule(_, _, _) |
-      ImportSymbol(_, _, _, _) | ImportAllSymbols(_, _) =>
-        (acc, true)
-    case Assign(List(lhs, _), _) if PrintLinearizedMutableEOWithCage.isSeqOfFields(lhs)  => (acc, true)
+      IfSimple(_, _, _, _) | While(_, _, _, _) | Suite(_, _) | Assign(List(_), _)
+      | Return(_, _) | FuncDef(_, _, _, _, _, _, Decorators(List()), _, _, _)
+      | NonLocal(_, _) | Pass(_) | Break(_) | Continue(_) | ImportModule(_, _, _)
+      | ImportSymbol(_, _, _, _) | ImportAllSymbols(_, _) =>
+      (acc, true)
+    case Assign(List(lhs, _), _) if PrintLinearizedMutableEOWithCage.isSeqOfFields(lhs) =>
+      (acc, true)
     case ClassDef(_, List(), body, Decorators(List()), _) => {
       val (Suite(defs, _), _) = SimplePass.procStatement(SimplePass.unSuite)(body, SimplePass.Names(HashMap()))
       assert(defs.forall { case Assign(List(Ident(_, _), _), _) => true case _ => false })
@@ -182,7 +184,7 @@ object SimpleAnalysis {
   private def assertExpressionIsSimplified(acc : Unit, e : T) : Unit = e match {
     case FreakingComparison(List(_), List(_, _), _) => ()
     case Star(_, _) | DoubleStar(_, _) | CollectionComprehension(_, _, _, _) | DictComprehension(_, _, _)
-         | CallIndex(false, _, _, _) | FreakingComparison(_, _, _) | AnonFun(_, _, _, _, _) =>
+      | CallIndex(false, _, _, _) | FreakingComparison(_, _, _) | AnonFun(_, _, _, _, _) =>
       throw new RuntimeException("these must never happen after all passes: " + PrintPython.printExpr(e))
     case _ => ()
   }
