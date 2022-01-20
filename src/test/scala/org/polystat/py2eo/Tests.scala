f6cc5e89456fc69de26d6b9bc28a7c7bccd2b4c6
Petr Babanov <petr.babanov@softcom.su>
2022-01-20 15:48:07 +0300

    fix spaces

--- src/test/scala/org/polystat/py2eo/Tests.scala
+++ src/test/scala/org/polystat/py2eo/Tests.scala
@@ -248,16 +248,16 @@ class Tests {
     println("Version of python is:")
     s"$python --version" !
 
-	// todo: test_named_expressions.py uses assignment expressions which are not supported.
-	// test_os leads to a strange error with inode numbers on the rultor server only
-	// supporting them may take several days, so this feature is currently skipped
+    // todo: test_named_expressions.py uses assignment expressions which are not supported.
+    // test_os leads to a strange error with inode numbers on the rultor server only
+    // supporting them may take several days, so this feature is currently skipped
 
-	// "test_strtod.py", todo: what's the problem here???
-	// "test_zipimport_support.py", todo: what's the problem here???
-	// "test_zipfile64.py" works for more 2 minutes, too slowm
-	// test_sys.py just hangs the testing with no progress (with no CPU load)
-	// test_dis.py, test*trace*.py are not supported, because they seem to compare line numbers, which change after printing
-	// many test for certain libraries are not present here, because these libraries are not installed by default in the CI
+    // "test_strtod.py", todo: what's the problem here???
+    // "test_zipimport_support.py", todo: what's the problem here???
+    // "test_zipfile64.py" works for more 2 minutes, too slowm
+    // test_sys.py just hangs the testing with no progress (with no CPU load)
+    // test_dis.py, test*trace*.py are not supported, because they seem to compare line numbers, which change after printing
+    // many test for certain libraries are not present here, because these libraries are not installed by default in the CI
 
 //    val test = List("test_statistics.py").map(name => new File(dirName + "/" + name))
     val test = dir.listFiles().toList
