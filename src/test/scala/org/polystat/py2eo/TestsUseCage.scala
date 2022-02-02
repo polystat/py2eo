57590a909082f8997908a7996510f09536a6918a
Andrew Goroshko <angoran16@gmail.com>
2022-02-02 23:00:49 +0300

    refactored test holder

--- src/test/scala/org/polystat/py2eo/TestsUseCage.scala
+++ src/test/scala/org/polystat/py2eo/TestsUseCage.scala
@@ -1,55 +1,55 @@
 package org.polystat.py2eo
 
 import org.junit.Test
-import org.junit.runners.Parameterized.Parameters
 import org.yaml.snakeyaml.Yaml
 
 import java.io.{File, FileInputStream}
 import java.nio.file.{Files, Path, Paths}
 
 class TestsUseCage extends Commons {
-  @Parameters def parsePython():collection.mutable.ArrayBuffer[YamlItem] = {
-    val res = collection.mutable.ArrayBuffer[YamlItem]()
-    Files.walk(Paths.get(testsPrefix)).filter((p: Path) => p.toString.endsWith(".yaml")).forEach((p: Path) => {
-      val testHolder = new File(p.toString)
-      val src = new FileInputStream(testHolder)
-      val yaml = new Yaml()
-      val yamlObj = yaml.load(src).asInstanceOf[java.util.Map[String, Any]]
-      res.addOne(new YamlItem(p,yamlObj))
-    })
-    res
+  def getYamlStr(path:String): String ={
+    val testHolder = new File(path)
+    val src = new FileInputStream(testHolder)
+    val yaml = new Yaml()
+    val yamlObj = yaml.load(src).asInstanceOf[java.util.Map[String, Any]]
+    yamlObj.get("python").asInstanceOf[String]
   }
 
   @Test def trivialTest():Unit = {
-    useCageRunner("trivial",simpleTest = false)
+    val test = testsPrefix + "trivial.yaml"
+    useCageHolder(test,getYamlStr(test))
   }
 
   @Test def simplestClassTest():Unit = {
-    useCageRunner("simplestClass",simpleTest = false)
+    val test = testsPrefix + "simplestClass.yaml"
+    useCageHolder(test,getYamlStr(test))
   }
 
   @Test def myListTest():Unit = {
-    useCageRunner("myList",simpleTest = false)
+    val test = testsPrefix + "myList.yaml"
+    useCageHolder(test,getYamlStr(test))
   }
 
   @Test def xTest():Unit = {
-    useCageRunner("x",simpleTest = false)
+    val test = testsPrefix + "x.yaml"
+    useCageHolder(test,getYamlStr(test))
   }
 
   @Test def whileCheckTest():Unit = {
-    useCageRunner("whileCheck",simpleTest = true)
+    Files.walk(Paths.get(testsPrefix + "simple_tests/whileCheck/")).filter((p: Path) => p.toString.endsWith(".yaml")).forEach((p: Path) => {
+      useCageHolder(p.toString, getYamlStr(p.toString))
+    })
   }
 
   @Test def ifCheck():Unit = {
-    useCageRunner("ifCheck",simpleTest = true)
+    Files.walk(Paths.get(testsPrefix + "simple_tests/ifCheck/")).filter((p: Path) => p.toString.endsWith(".yaml")).forEach((p: Path) => {
+      useCageHolder(p.toString, getYamlStr(p.toString))
+    })
   }
 
   @Test def assignCheck():Unit = {
-    useCageRunner("assignCheck",simpleTest = true)
-  }
-
-  def useCageRunner(fName:String,simpleTest:Boolean):Unit = {
-    val testData = getTestEntry(parsePython(),fName,simpleTest)
-    useCageHolder(testData._1, testData._2)
+    Files.walk(Paths.get(testsPrefix + "simple_tests/assignCheck/")).filter((p: Path) => p.toString.endsWith(".yaml")).forEach((p: Path) => {
+      useCageHolder(p.toString, getYamlStr(p.toString))
+    })
   }
 }
