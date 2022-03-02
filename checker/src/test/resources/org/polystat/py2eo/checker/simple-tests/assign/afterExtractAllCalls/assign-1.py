ed47570766968a33b6bbc09aadea5cfd6c8fcd71
bugdea1er <bugdealer@icloud.com>
2022-03-02 14:24:27 +0300

    Removed generated files

--- checker/src/test/resources/org/polystat/py2eo/checker/simple-tests/assign/afterExtractAllCalls/assign-1.py
+++
@@ -1,4 +0,0 @@
-def xassignTest1(): # 1:0-4:25
-    xa = 1  # 2:4-2:8
-    xb2 = 2  # 3:4-3:9
-    return True # 4:4-4:14
--- checker/src/test/resources/org/polystat/py2eo/checker/simple-tests/if/afterExtractAllCalls/if-3.py
+++
@@ -1,8 +0,0 @@
-def xconditionalCheck3(): # 1:0-6:-1
-    xa = 4  # 2:4-2:8
-    xb2 = 2  # 3:4-3:9
-    e0 = (xa != xb) # 4:9-4:14
-    if (e0): # 4:32-6:-1 
-        return True # 5:8-5:18
-    else: # 4:4-6:-1
-        pass # 4:4-6:-1
