470d7558816d46313d4775725ac72056746590c1
Andrew Goroshko <angoran16@gmail.com>
2022-05-12 12:47:27 +0300

    removed junk code

--- transpiler/src/test/scala/org/polystat/py2eo/transpiler/Counter.scala
+++ transpiler/src/test/scala/org/polystat/py2eo/transpiler/Counter.scala
@@ -6,47 +6,36 @@ import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 import org.junit.runners.Parameterized.Parameters
 import org.polystat.py2eo.transpiler.Main.writeFile
-import org.yaml.snakeyaml.Yaml
-import org.yaml.snakeyaml.error.YAMLException
 
-import java.io.{File, FileInputStream, FileWriter}
+import java.io.File
 import java.nio.file.StandardCopyOption.REPLACE_EXISTING
 import java.nio.file.{Files, Path, Paths}
 import java.{lang => jl, util => ju}
 import scala.language.postfixOps
 import scala.sys.process.Process
 
+object Counter extends Commons {
+  @Parameters def parameters: ju.Collection[Array[jl.String]] = collect("simple-tests")
+}
 
 @RunWith(value = classOf[Parameterized])
-class Counter(path: jl.String) {
-  val testsPrefix: String = getClass.getResource("").getFile
+class Counter(path: jl.String) extends Commons {
   private val runEOPath = Paths.get(".").toAbsolutePath.getParent.getParent + "/runEO"
 
-  def yaml2python(f: File): String = {
-    val yaml = new Yaml()
-    val map = yaml.load[java.util.Map[String, String]](new FileInputStream(f))
-
-    map.get("python")
-  }
-
-  def useCageHolder(test: File): Unit = {
+  @Test def testDef(): Unit = {
+    val test = new File(path)
     val yamlData = yaml2python(test)
 
-    val res = Transpile(test.getName.replace(".yaml", ""), yamlData)
-
-    res match {
+    Transpile(test.getName.replace(".yaml", ""), yamlData) match {
       case None => fail(s"could not transpile ${test.getName}");
       case Some(transpiled) =>
-        val path = writeFile(
-          test, "genCageEO", ".eo", transpiled
-        )
+        val path = writeFile(test, "genCageEO", ".eo", transpiled)
 
         if (!run(path)) {
           fail(s"could not run EO ${test.getName}")
         }
     }
   }
-
 
   private def run(file: File): Boolean = {
     val result = Files.copy(file.toPath, Path.of(s"$runEOPath/test.eo"), REPLACE_EXISTING)
@@ -56,34 +45,4 @@ class Counter(path: jl.String) {
 
     ret
   }
-
-  @Test def testDef(): Unit = {
-    useCageHolder(new File(path))
-  }
 }
-
-
-object Counter {
-  @Parameters def parameters: ju.Collection[Array[jl.String]] = {
-    val testsPrefix = System.getProperty("user.dir") + "/src/test/resources/org/polystat/py2eo/transpiler"
-
-    val res = collection.mutable.ArrayBuffer[String]()
-    val simpleTestsFolder = new File(testsPrefix + File.separator + "simple-tests" + File.separator)
-    Files.walk(simpleTestsFolder.toPath).filter((p: Path) => p.toString.endsWith(".yaml")).forEach((p: Path) => {
-      val testHolder = new File(p.toString)
-
-      try {
-        res.addOne(p.toString)
-      } catch {
-        case e: YAMLException => fail(s"Couldn't parse ${testHolder.getName} file with error ${e.getMessage}")
-        case e: ClassCastException => fail(s"Couldn't parse ${testHolder.getName} file with error ${e.getMessage}")
-      }
-    })
-
-
-    val list = new ju.ArrayList[Array[jl.String]]()
-    res.foreach(n => list.add(Array(n)))
-    list
-  }
-}
-
--- transpiler/src/test/scala/org/polystat/py2eo/transpiler/TestsSimple.scala
+++ transpiler/src/test/scala/org/polystat/py2eo/transpiler/TestsSimple.scala
@@ -1,50 +1,18 @@
 package org.polystat.py2eo.transpiler
 
-import org.junit.Assert.fail
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 import org.junit.runners.Parameterized.Parameters
-import org.yaml.snakeyaml.Yaml
-import org.yaml.snakeyaml.error.YAMLException
 
-import java.io.{File, FileInputStream}
-import java.nio.file.{Files, Path}
+import java.io.File
 import java.{lang => jl, util => ju}
+
+object TestsSimple extends Commons {
+  @Parameters def parameters: ju.Collection[Array[jl.String]] = collect("simple-tests", needsFiltering = true)
+}
 
 @RunWith(value = classOf[Parameterized])
 class TestsSimple(path: jl.String) extends Commons {
-  @Test def testDef(): Unit = {
-    useCageHolder(new File(path))
-  }
-}
-
-object TestsSimple {
-  @Parameters def parameters: ju.Collection[Array[jl.String]] = {
-    val testsPrefix = System.getProperty("user.dir") + "/src/test/resources/org/polystat/py2eo/transpiler"
-
-    val res = collection.mutable.ArrayBuffer[String]()
-    val simpleTestsFolder = new File(testsPrefix + File.separator + "simple-tests" + File.separator)
-    Files.walk(simpleTestsFolder.toPath).filter((p: Path) => p.toString.endsWith(".yaml")).forEach((p: Path) => {
-      val testHolder = new File(p.toString)
-      val yaml = new Yaml()
-      val map = yaml.load[java.util.Map[String, String]](new FileInputStream(testHolder))
-
-      try {
-        if (map.containsKey("enabled") && map.getOrDefault("enabled", "false").asInstanceOf[Boolean]){
-          res.addOne(p.toString)
-        }else{
-          println(s"The test ${testHolder.getName} is disabled")
-        }
-      } catch {
-        case e: YAMLException => fail(s"Couldn't parse ${testHolder.getName} file with error ${e.getMessage}")
-        case e: ClassCastException => fail(s"Couldn't parse ${testHolder.getName} file with error ${e.getMessage}")
-      }
-    })
-
-
-    val list = new ju.ArrayList[Array[jl.String]]()
-    res.foreach(n => list.add(Array(n)))
-    list
-  }
+  @Test def testDef(): Unit = useCageHolder(new File(path))
 }
