4c4a5dd16f3744a557df6d7acdba8d490df3dc67
Ilya Andreev <bugdealer@icloud.com>
2022-06-21 17:05:50 +0300

    Updated DjangoTest.scala

---
+++ transpiler/src/test/scala/org/polystat/py2eo/transpiler/DjangoTest.scala
@@ -0,0 +1,37 @@
+package org.polystat.py2eo.transpiler
+
+import org.junit.{Ignore, Test}
+import org.polystat.py2eo.parser.Statement
+import org.polystat.py2eo.transpiler.Common.dfsFiles
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
+import scala.language.postfixOps
+import scala.reflect.io.{Directory, File}
+import scala.sys.process.Process
+
+
+class DjangoTest extends Commons {
+
+  @Test
+  def genUnsupportedDjango(): Unit = {
+    val root = Directory(testsPrefix)
+    val djangoDir = Directory(root / "django")
+    if (!djangoDir.exists) {
+      assert(0 == Process("git clone https://github.com/django/django", root.jfile).!)
+    }
+
+    val tests = djangoDir.deepFiles.filter(_.extension == "py")
+    val futures = for {test <- tests} yield {
+      Future {
+        Transpile(test.stripExtension, test.slurp) match {
+          case None => println(s"failed to transpile ${test.name}")
+          case Some(transpiled) => writeFile(test.jfile, "genUnsupportedEO", ".eo", transpiled)
+        }
+      }
+    }
+
+    for (f <- futures) Await.result(f, Duration.Inf)
+  }
+}
