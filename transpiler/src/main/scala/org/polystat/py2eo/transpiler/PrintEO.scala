df211234eb79bb054836cff0fa824acfb3cfcc4f
dours <akadours2@gmail.com>
2022-07-26 23:15:43 +0300

    add a hack to support complicated `in` and enable a test:
    each object, which supports `is` should have both `__contains__`, which is a regular python method and `contains-hack`, which does the same but is just a EO method. This is needed because currently it is only possible to use an identifier to address a python method. That is, calling "sring literal".__contains__(...) does not work now because of a limitation of a pass, which adds `self` to each method call. On the other hand, calling EO method is possible in any context, like `(pystring "sring literal").contains-hack ...` is ok.

--- transpiler/src/main/scala/org/polystat/py2eo/transpiler/PrintEO.scala
+++ transpiler/src/main/scala/org/polystat/py2eo/transpiler/PrintEO.scala
@@ -89,7 +89,7 @@ object PrintEO {
         val r1 = Field(r, "x__id__", ann.pos)
         printExpr(SimpleComparison(if (op == Compops.Is) Compops.Eq else Compops.Neq, l1, r1, ann.pos))
       case SimpleComparison(op, l, r, _) if op == Compops.In =>
-        orb + e(r) + ".x__contains__" + space + e(l) + crb
+        orb + e(r) + ".contains-hack" + space + e(l) + crb
       case SimpleComparison(op, l, r, _) => orb + e(l) + "." + compop(op) + space + e(r) + crb
       case FreakingComparison(List(op), List(l, r), _) => orb + e(l) + "." + compop(op) + space + e(r) + crb
       case LazyLAnd(l, r, _) =>  orb + e(l) + ".and " + e(r) + crb
