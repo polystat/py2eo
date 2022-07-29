ec509b2313f685eec14f452d403e44139a151798
rultor <rultor@users.noreply.github.com>
2022-07-29 18:51:29 +0000

    Deploying to gh-pages from @ polystat/py2eo@f7fdff84a66f544818d8ab5250796cbae2d533a4 🚀

--- conditional-2.py
+++ conditional-2.py
@@ -1,4 +1,4 @@
-def cond2(): # 1:0-4:31
+def cond2(): # 1:0-4:28
     b = 2  # 2:4-2:8
     a = (1  == b ) if "asd" else 1  # 3:4-3:33
-    return (a == 1  ) # 4:4-4:20
+    return (not a) # 4:4-4:17
