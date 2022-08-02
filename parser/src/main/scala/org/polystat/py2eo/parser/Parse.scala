beb9dbd6c484a4a643af20535318a4a7c25b3db1
bugdea1er <bugdealer@icloud.com>
2022-08-02 17:03:02 +0300

    Update Parse.scala

--- parser/src/main/scala/org/polystat/py2eo/parser/Parse.scala
+++ parser/src/main/scala/org/polystat/py2eo/parser/Parse.scala
@@ -1,42 +1,45 @@
 package org.polystat.py2eo.parser
 
-import org.antlr.v4.runtime.misc.ParseCancellationException
+import org.antlr.v4.runtime.{ANTLRInputStream, CommonTokenStream}
 
 import java.io.File
 import scala.io.Source
-import org.antlr.v4.runtime.{ANTLRInputStream, CommonTokenStream}
+import scala.util.Try
 
 object Parse {
 
+  /** Debug printer that does nothing */
+  private val nullDebugPrinter = (_: Statement.T, _: String) => ()
+
+  /** Parses the given .py file */
+  def apply(file: File): Option[Statement.T] = apply(file, nullDebugPrinter)
+
+  /** Parses the given input string */
+  def apply(input: String): Option[Statement.T] = apply(input, nullDebugPrinter)
+
+  /** Parses the given .py file and prints debug output with the given debug printer */
   def apply(file: File, debugPrinter: (Statement.T, String) => Unit): Option[Statement.T] = {
-    val stmt = Parse(file)
-    stmt match { case Some(stmt) => debugPrinter(stmt, "afterParser") case None => () }
-    stmt
-  }
-
-  def apply(input: String, debugPrinter: (Statement.T, String) => Unit): Option[Statement.T] = {
-    val stmt = Parse(input)
-    stmt match { case Some(stmt) => debugPrinter(stmt, "afterParser") case None => () }
-    stmt
-  }
-
-  def apply(file: File): Option[Statement.T] = {
     assert(file.getName.endsWith(".py"))
     val input = Source.fromFile(file)
-    Parse(input.mkString)
+    apply(input.mkString, debugPrinter)
   }
 
-  def apply(input: String): Option[Statement.T] = {
-    try {
+  /** Parses the given input string and prints debug output with the given debug printer */
+  def apply(input: String, debugPrinter: (Statement.T, String) => Unit): Option[Statement.T] = {
+    val parsed = parseToOption(input)
+    parsed.foreach(stmt => debugPrinter(stmt, "afterParser"))
+
+    parsed
+  }
+
+  /** Calls antlr parser and maps the result to the custom AST */
+  private def parseToOption(input: String): Option[Statement.T] = {
+    Try {
       val inputStream = new ANTLRInputStream(input)
       val lexer = new PythonLexer(inputStream)
       val tokenStream = new CommonTokenStream(lexer)
       val parser = new PythonParser(tokenStream)
-      Some(MapStatements.mapFile(parser.file))
-    }
-    catch {
-      case _ : ParseCancellationException => None
-    }
+      MapStatements.mapFile(parser.file)
+    }.toOption
   }
-
 }
