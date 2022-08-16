20b00ce9ee321d11f4270fe02a3c05673aade37b
dours <akadours2@gmail.com>
2022-08-16 23:08:54 +0300

    support calling a method of some expression, not only of an identifier

--- transpiler/src/main/scala/org/polystat/py2eo/transpiler/ExpressionPasses.scala
+++ transpiler/src/main/scala/org/polystat/py2eo/transpiler/ExpressionPasses.scala
@@ -57,6 +57,29 @@ object ExpressionPasses {
       e match {
         case CallIndex(true, what@Field(obj@Ident(_, _), fname, fann), args, ann) =>
           (Left(CallIndex(true, what, (None, obj) :: args, ann.pos)), ns)
+        case CallIndex(true, what@Field(obj, fname, fann), args, ann) =>
+          val (List(objName, idName), ns1) = ns(List("obj", "id"))
+          (
+            Right((
+              Suite(
+                List(
+                  Assign(List(Ident(objName, ann.pos), obj), ann.pos),
+                  Assign(List(
+                    Ident(idName, ann.pos),
+                    CallIndex(
+                      true,
+                      Field(Ident(objName, ann.pos), fname, fann),
+                      (None, Ident(objName, ann.pos)) :: args,
+                      ann.pos
+                    )
+                  ), ann.pos)
+                ),
+                ann.pos
+              ),
+              Ident(idName, ann.pos)
+            )),
+            ns1
+          )
         case CallIndex(isCall, Field(_, _, _), args, ann) => ??? // todo: must be implemented as above, but a bit more complicated
         case x : Any => (Left(x), ns)
       }
