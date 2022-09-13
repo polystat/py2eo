3c5957e6a5cfc819cb71ce615dc0bea9999af302
bugdea1er <bugdealer@icloud.com>
2022-09-14 01:05:22 +0300

    Stylefixes

--- parser/src/test/scala/org/polystat/py2eo/parser/TestParserPrinter.scala
+++ parser/src/test/scala/org/polystat/py2eo/parser/TestParserPrinter.scala
@@ -5,7 +5,7 @@ import org.junit.Test
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future}
+import scala.concurrent.{Await, Awaitable, Future}
 import scala.reflect.io.Directory
 import scala.sys.process.Process
 import scala.util.Properties
@@ -22,21 +22,30 @@ final class TestParserPrinter {
     Process(s"git clone $cpythonLink ${cpython.name}", directory.jfile).!!
     Process("git checkout v3.8.10", cpython.jfile).!!
 
-    val testsDir = Directory(cpython / "Lib" / "test")
-    val tests = testsDir.deepFiles.filter(_.extension == "py")
+    val testsDirectory = Directory(cpython / "Lib" / "test")
+    val tests = testsDirectory.deepFiles.filter(_.extension == "py")
 
     val futures = for {test <- tests} yield Future {
-      Parse(test.slurp).map(PrintPython.print).fold(fail())(test.writeAll(_))
+      Parse(test.slurp).map(PrintPython.print).fold(fail())(test writeAll _)
     }
 
-    for (f <- futures) Await.result(f, Duration.Inf)
+    futures foreach await
 
     assume(Properties.isMac || Properties.isLinux)
     Process("./configure", cpython.jfile).!!
 
-    val processorsNumber = Runtime.getRuntime.availableProcessors
-    println(s"have $processorsNumber processors")
-    Process(s"make -j ${processorsNumber + 2}", cpython.jfile).!!
+    val availableProcessors = sys.runtime.availableProcessors
+    println(s"have $availableProcessors processors")
+    Process(s"make -j ${availableProcessors + 2}", cpython.jfile).!!
     Process("make test", cpython.jfile).!!
+  }
+
+  /**
+   * Await and return the result (of type `T`) of an `Awaitable`.
+   *
+   * @param awaitable the `Awaitable` to be awaited
+   */
+  private def await[T](awaitable: Awaitable[T]): T = {
+    Await.result(awaitable, Duration.Inf)
   }
 }
