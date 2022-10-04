1de4f85eb083b412fc6cb04afc1885d105367d7b
dours <akadours2@gmail.com>
2022-10-04 18:32:02 +0300

    support the annotated-3.yaml test

--- transpiler/src/main/scala/org/polystat/py2eo/transpiler/AddExplicitConstructionOfCollection.scala
+++ transpiler/src/main/scala/org/polystat/py2eo/transpiler/AddExplicitConstructionOfCollection.scala
@@ -1,13 +1,18 @@
 package org.polystat.py2eo.transpiler
 
-import org.polystat.py2eo.parser.Expression.{CallIndex, CollectionCons, CollectionKind, DictCons, Ident, T}
+import org.polystat.py2eo.parser.Expression.{BoolLiteral, CallIndex, CollectionCons, CollectionKind, DictCons, Ident, T}
 
 object AddExplicitConstructionOfCollection {
   def apply(e : T) : T = {
     e match {
       case CollectionCons(kind, l, ann)
         if kind == CollectionKind.List || kind == CollectionKind.Tuple =>
-          CallIndex(true, Ident("xmyArray", e.ann.pos), List((None, e)), e.ann.pos)
+          CallIndex(
+            true,
+            Ident("xmyArray", e.ann.pos),
+            List((None, BoolLiteral(kind == CollectionKind.List, e.ann.pos)), (None, e)),
+            e.ann.pos
+          )
       case DictCons(_, _) | CollectionCons(CollectionKind.Set, _, _) =>
         CallIndex(true, Ident("xmyMap", e.ann.pos), List((None, e)), e.ann.pos)
       case _ => e
