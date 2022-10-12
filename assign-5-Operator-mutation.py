dfee6bc94d39118818bffd037e96352f18c01021
rultor <rultor@users.noreply.github.com>
2022-10-12 18:55:47 +0000

    Deploying to gh-pages from @ polystat/py2eo@ee037dbdd1166f14b92f09a912b9a3e1c737864f 🚀

--- assign-5-Operator-mutation.py
+++ assign-5-Operator-mutation.py
@@ -5,7 +5,7 @@ def test7_2_1(): # 1:0-12:82
     class c(): # 5:4-7:3
         attr = 1  # 6:8-6:15
     c.attr-=1  # 7:4-7:12
-    checkTargetC = (c.attr == 1  ) # 8:4-8:33
+    checkTargetC = (c.attr == 2  ) # 8:4-8:33
     a = [0 , 1 ] # 9:4-9:15
     a[0 ]-=1  # 10:4-10:11
     checkTargetSubscr = (a[0 ] == 1  ) # 11:4-11:37
