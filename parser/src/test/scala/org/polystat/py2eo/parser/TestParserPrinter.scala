90d9eb99224fb1ce6314814307b6a8c45cfdb7e5
dours <akadours2@gmail.com>
2022-10-18 10:35:35 +0300

    use junit5

--- parser/src/test/scala/org/polystat/py2eo/parser/TestParserPrinter.scala
+++ parser/src/test/scala/org/polystat/py2eo/parser/TestParserPrinter.scala
@@ -1,7 +1,7 @@
 package org.polystat.py2eo.parser
 
-import org.junit.Assert.fail
-import org.junit.{After, Test}
+import org.junit.jupiter.api.Assertions.fail
+import org.junit.jupiter.api.{Test, AfterAll}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
@@ -58,7 +58,7 @@ final class TestParserPrinter {
     Process("make test", cpython.jfile).!!
   }
 
-  @After def cleanup(): Unit = {
+  @AfterAll def cleanup(): Unit = {
     directory.deleteRecursively
   }
 
